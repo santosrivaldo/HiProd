e7f8902abc7e42842cb2ead64102196f041bd2e3
santosrivaldo <77171407+santosrivaldo@users.noreply.github.com>
2025-09-19 10:46:26 -0300

    Correção vite

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'hiprod.grupohi.com.br',
+    host: '0.0.0.0',
     port: 5000,
     strictPort: true,
-    allowedHosts: ['hiprod.grupohi.com.br']
+    allowedHosts: ['hiprod.grupohi.com.br', '.replit.dev']
   }
 })
